866173f6813309c2da15f1ba93cec93489054894
Dmitry Sychev <dimtrisythev@gmail.com>
2023-03-08 09:20:33 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="font-mono mx-10 m-10">rонструктор калькулятора</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
